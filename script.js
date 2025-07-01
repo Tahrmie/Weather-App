bfd6a92f59b5421b91166251c67be7d715039554
Chioma777 <claireiweama@gmail.com>
2025-07-01 08:49:50 +0100

    Update

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 const apiKey = "f4c40c9b4625a5b75db5de3814131da0";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metrics";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  let data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+  document.querySelector(".temp").innerHTML =
+    Math.round(data.main.temp) + "&#8451;";
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
